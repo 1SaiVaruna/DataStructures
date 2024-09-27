19f4a07b5c8b45194630ca769967dfc0d28aab6c
1SaiVaruna <sai030804@gmail.com>
2024-09-27 12:12:05 +0100

    Added 'TVShow' class & shows in 'Program' class

---
+++ DataStructures/TVShow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class TVShow
+    {
+        public string title = "Untitled";
+        public int releaseYear;
+        public double price;
+        public MovieRating rating;
+
+        public TVShow(string title, int releaseYear, double price, MovieRating rating)
+        {
+            this.title = title;
+            this.releaseYear = releaseYear;
+            this.price = price;
+            this.rating = rating;
+        }
+
+        public override string ToString()
+        {
+            return title + " (" + releaseYear + ") Rated " + rating + " ... £" + price.ToString("0.00");
+        }
+    }
+}
